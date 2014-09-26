9efefa7787402898fc0b063a7a99349e060e7d81
Vilcea <iosifvilcea@gmail.com>
2014-09-26 15:33:41 -0400

    list of syscalls added

--- tracetest.c
+++ tracetest.c
@@ -2,6 +2,29 @@
 #include <string.h>
 #include <unistd.h>
 
+//  open()
+//  read()
+//  write()
+//  close()
+//  fork()
+//  exec()
+//  wait()
+//  getTime()
+//  getpid()
+//  getcwd()
+//
+//  changeMod()
+//  makedir()
+//  removeDir()
+//  rename()
+//  exit()
+//  sysInfo()
+//  kill()
+//  sethostname()
+//  sleep()
+//  chroot()
+//
+
 int main(int argc, const char *argv[])
 {
     
