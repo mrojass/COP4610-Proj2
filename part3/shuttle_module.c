8bcf9d1ba2a34058a971eca2f3fe458fe814044a
Vilcea <iosifvilcea@gmail.com>
2014-10-20 00:37:08 -0400

    Basic Module

--- part3/shuttle_module.c
+++ part3/shuttle_module.c
@@ -18,5 +18,25 @@ static int shuttle_exit(void)
     return 0;
 }
 
+static proc_open(struct inode *inode, struct file *file){
+    return single_open(file, proc_show, NULL);
+}
+
+static const struct file_operations proc_fops={
+    .owner = THIS_MODULE,
+    .open = proc_open,
+    .read = seq_read,
+    .release = single_release,
+};
+
+static int _init date_proc_init(void){
+    proc_create("terminal", 0, NULL, &proc_fops);
+    return 0;
+}
+
+static void _exit date_proc_exit(void){
+    remove_proc_entry("terminal", NULL);
+}
+
 module_init(shuttle_init);
 module_exit(shuttle_exit);
